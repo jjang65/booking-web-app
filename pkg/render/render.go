1a47b0ca8d92bbd935f06301308d226d8faebc08
jjang65 <jjy2zzbai2@gmail.com>
2022-04-21 12:04:15 -0500

    Form with CSRF token

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/jjang65/booking-web-app/pkg/config"
 	"github.com/jjang65/booking-web-app/pkg/models"
+	"github.com/justinas/nosurf"
 	"html/template"
 	"log"
 	"net/http"
@@ -64,12 +65,13 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 	return myCache, nil
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	// Define templateCache
 	var tc map[string]*template.Template
 
@@ -94,7 +96,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 	// Put parsed template into bytes in memory
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	// Execute applies a parsed template to the specified data object, writing the output to wr.
 	_ = t.Execute(buf, td)
