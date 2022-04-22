bd6aa45f1fbe251979c6a0d8f87b513f49b0757a
jjang65 <jjy2zzbai2@gmail.com>
2022-04-21 19:42:08 -0500

    AJAX

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/jjang65/booking-web-app/pkg/config"
 	"github.com/jjang65/booking-web-app/pkg/models"
 	"github.com/jjang65/booking-web-app/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -76,6 +78,26 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	end := r.Form.Get("end")
 
 	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availability and sends JSON response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		true,
+		"available!",
+	}
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(out))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Contact renders the contact page
