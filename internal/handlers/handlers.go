f0dcb1b9c2bfabd45b319e327972c198286e0506
jjang65 <jjy2zzbai2@gmail.com>
2022-04-22 20:15:51 -0500

    required, length, and email validators

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -55,8 +55,12 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
@@ -74,8 +78,20 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		Phone:     r.Form.Get("phone"),
 		Email:     r.Form.Get("email"),
 	}
+	// Create an empty form
 	form := forms.New(r.PostForm)
-	form.Has("first_name", r)
+
+	// Validate required field
+	//form.Has("first_name", r)
+
+	// Validate fields to check if they are not empty
+	form.Required("first_name", "last_name", "email")
+
+	// Validate if field has at least 3 chars
+	form.MinLength("first_name", 3, r)
+
+	// Validate email field
+	form.IsEmail("email")
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
