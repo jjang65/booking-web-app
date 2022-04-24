ca1fadd00f5734bc9446d0a79646ef16630e2cd5
jjang65 <jjy2zzbai2@gmail.com>
2022-04-24 00:10:23 -0500

    testing render with session data

---
+++ internal/render/setup_test.go
@@ -0,0 +1,37 @@
+package render
+
+import (
+	"encoding/gob"
+	"github.com/alexedwards/scs/v2"
+	"github.com/jjang65/booking-web-app/internal/config"
+	"github.com/jjang65/booking-web-app/internal/models"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	// Store Reservation type in the session
+	// gob is standard library
+	gob.Register(models.Reservation{})
+
+	// Change this to ture when in production
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true // Session will persist even after closing a tab
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false // We're testing so should be not in https
+
+	testApp.Session = session
+
+	// Assign app declared in render.go to testApp created above
+	app = &testApp
+
+	os.Exit(m.Run())
+}
