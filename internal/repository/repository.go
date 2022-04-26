ceb7e74a1842e1a1ecddb9b53e8afd59ba641abe
jjang65 <jjy2zzbai2@gmail.com>
2022-04-26 16:58:51 -0500

    fix

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -10,5 +10,5 @@ type DatabaseRepo interface {
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
-	SearchAvailabilityByDates(start, end time.Time) (bool, error)
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
