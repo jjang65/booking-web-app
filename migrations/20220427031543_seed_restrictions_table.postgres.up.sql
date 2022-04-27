0c472350efc81dcc9f88c907c22026719b0d373f
jjang65 <jjy2zzbai2@gmail.com>
2022-04-26 22:16:58 -0500

    create migrations

---
+++ migrations/20220427031543_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO restrictions (restriction_name, created_at, updated_at)
+VALUES ('Reservation', '2022-04-25 00:00:00', '2022-04-25 00:00:00'),
+       ('Owner Block', '2022-04-25 00:00:00', '2022-04-25 00:00:00');
